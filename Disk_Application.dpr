12c02a2cb0cb459d291a7554e200fa9988277a59
Mxovela <yanela692@gmail.com>
2024-05-31 12:13:08 +0200

    Last Commit

---
+++ Disk_Application.dpr
@@ -0,0 +1,14 @@
+program Disk_Application;
+uses
+  Forms,
+  ServerModule in 'ServerModule.pas' {UniServerModule: TUniGUIServerModule},
+  MainModule in 'MainModule.pas' {uni: TUniGUIMainModule},
+  Main in 'Main.pas' {MainForm: TUniForm};
+
+{$R *.res}
+begin
+  ReportMemoryLeaksOnShutdown := True;
+  Application.Initialize;
+  TUniServerModule.Create(Application);
+  Application.Run;
+end.
